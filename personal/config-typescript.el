ea5c8ff41793944d5f4d4ae7303c733e689385f6
Dan Barbarito <dan@barbarito.me>
2024-11-20 12:34:27 -0500

    js

--- personal/config-typescript.el
+++ personal/config-typescript.el
@@ -12,8 +12,8 @@
   (setq typescript-indent-level 2))
 
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . typescript-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . tsx-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
 
 (add-hook 'typescript-ts-mode-hook 'my--init-ts-mode)
